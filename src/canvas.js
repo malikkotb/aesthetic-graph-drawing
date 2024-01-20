cedfa29892873f6ae8b3c5443939812e70db4ac1
Malik Kotb <malikkotb@icloud.com>
2024-01-20 13:59:09 +0100

    draw lines and bezier curves

--- src/canvas.js
+++ src/canvas.js
@@ -1,37 +1,24 @@
-window.addEventListener('load', () => {
-    const canvas = document.querySelector("#canvas");
-    // define what context we are working in
-    /** @type {CanvasRenderingContext2D} */
-    const ctx = canvas.getContext("2d");
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("#canvas");
+  // define what context we are working in
+  /** @type {CanvasRenderingContext2D} */
+  const ctx = canvas.getContext("2d");
 
-    // resizing
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+  // resizing
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
 
-    // Draw Rectangles
-    // source: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Applying_styles_and_colors?retiredLocale=de
-    ctx.strokeStyle = "red"; // the style always affecs the next context rendered
-    ctx.lineWidth = 5;
-    ctx.strokeRect(100, 100, 200, 500)
+  // Draw Lines
+  ctx.beginPath();
+  ctx.moveTo(200, 80); // starting position of line
+  ctx.lineTo(300, 80);
+  ctx.lineTo(300, 150);
+  ctx.closePath(); // connects remaining path
+  ctx.stroke();
 
-    ctx.strokeStyle = "blue";
-    ctx.lineWidth = 1;
-    ctx.strokeRect(200, 200, 200, 500)
-
-
-    // Draw Lines 
-    ctx.beginPath()
-    ctx.moveTo(100, 80) // starting position of line
-    ctx.lineTo(200, 80)
-    ctx.stroke()
-
-
-    // Draw beziercurves
-    // ctx.bezierCurveTo()
-
-})
-
-// window.addEventListener('resize', () => {
-//     canvas.height = window.innerHeight;
-//     canvas.width = window.innerWidth;
-// })
+  // Draw beziercurves
+  ctx.beginPath();
+  ctx.moveTo(30, 30);
+  ctx.bezierCurveTo(120, 160, 180, 10, 220, 140);
+  ctx.stroke();
+});
