95502fe6e0ce65ac8676c03b31b5bc6f5b387f69
Malik Kotb <malikkotb@icloud.com>
2024-02-14 08:46:44 +0100

    cell border calculations

--- src/astar/grid.js
+++ src/astar/grid.js
@@ -20,9 +20,6 @@ export class Grid {
     //  -> extract positions of nodes from the user-input and make all those tiles/cells where
     // the nodes are rendered on -> an obstacle
 
-
-
-
     // Top-Left Corner (Given): (nodeX, nodeY)
 
     // Top-Right Corner:
@@ -38,30 +35,70 @@ export class Grid {
     for (let x = 0; x < width; x++) {
       this.grid[x] = [];
       for (let y = 0; y < height; y++) {
-        const cellX = x*10;
-        const cellY = y*10;
-        const {x:nodeX ,y:nodeY, width: nodeWidth, height: nodeHeight} = nodeCoordinates[0]
-        
-        // determine marked-state of tile and pass that into the constructor of Cell
+        const cellX = x * 10;
+        const cellY = y * 10;
+
         //TODO: add functionality for MARKED-status for A* (start, end, open, closed)
         let marked = "";
 
+        nodeCoordinates.forEach((node) => {
+          const {
+            x: nodeX,
+            y: nodeY,
+            width: nodeWidth,
+            height: nodeHeight,
+          } = node;
 
-      
-        if (cellX >= nodeX/10 && cellX < nodeX/10 + nodeWidth/10 && cellY >= nodeY/10 && cellY < nodeY/10 + nodeHeight/10) {
-        //   // The cell is within the node's bounds or at its corners
-          console.log("MARK CELL");        
-          console.log("cellX, cellY ", cellX, cellY);
-          console.log("nodeX, nodeY ", nodeX/10, nodeY/10); // divide node-position by grid width (# of cells)
-          marked = "OBSTACLE";
+          console.log("");
+          if (cellX !== 0 && cellX !== 40) {
 
+            console.log("cellX, cellY ", cellX, cellY);
+            console.log("nodeX, nodeY ", nodeX / 10, nodeY / 10);
+          }
+
+          if (cellX >= nodeX / 10) {
+            console.log("cellX >= nodeX ? ", cellX, nodeX / 10);
+          }
+          
+          if (cellY >= nodeY / 10) {
+            console.log("cellY >= nodeY ? ", cellY, nodeY / 10);
+          }
+
+
+          // if (
+          //   cellX_10 >= nodeX_15  &&
+          //   ((cellX_10 <= (nodeX_15 + nodeWidth)) || (cellX_10 <= (nodeX_15 - nodeWidth))) &&
+          //   cellY_10 >= nodeY_15 &&
+          //   cellY_10 <= (nodeY_15 + nodeHeight)
+          // ) 
+
+          if (
+            cellX >= nodeX / 10 &&
+            ((cellX <= (nodeX + nodeWidth) / 10) || (cellX >= (nodeX - nodeWidth) / 10) ) 
+            && 
+            // (cellY >= nodeY / 10)
+            // &&
+            ((cellY <= (nodeY + nodeWidth) / 10) || (cellY >= (nodeY - nodeWidth) / 10) ) 
+
+            // cellX <= ((nodeX + nodeWidth) || (nodeX - nodeWidth)) / 10 &&
+            // cellY >= nodeY / 10 &&
+            // cellY <= (nodeY + nodeHeight) / 10
+          ) {
+            marked = "OBSTACLE";
+            console.log("OBSTACLE");
+            // console.log("cellX, cellY ", cellX, cellY);
+            // console.log("nodeX, nodeY ", nodeX / 10, nodeY / 10); // divide node-position by grid width (# of cells)
+          }
+        });
+
+        if (cellX === 30) {
+          return;
         }
+
         this.grid[x][y] = new Cell(context, x, y, 100, 100, marked);
       }
     }
   }
-
-  
 
   // Method to get a cell at a specific coordinate
   getCell(x, y) {
