6db1fb9815a7ed481288cb91470cb782ef06ff13
Malik Kotb <malikkotb@icloud.com>
2024-02-15 11:55:45 +0100

    grid markers

--- src/astar/grid.js
+++ src/astar/grid.js
@@ -10,26 +10,15 @@ export class Grid {
     this.grid = [];
     this.nodeCoordinates = nodeCoordinates; // list of coordinates of nodes
 
-    // TODO: function for setting unwalkable/obstacle tiles
     // Steps before running A*
     // 1. Create the grid (not marked yet; just the grid)
     // 2. check the configuration of states (they are given in coordinates)
-    // 3. if coordinates of a state is inside a specific cell then mark that cell as obstacle
-    // 4. (figure out way of marking other cells before rendering each edge to make edge follow aesthetic path)
-    // 5. add a layer on top of that grid representing the unwalkable areas or obstacles
-    //  -> extract positions of nodes from the user-input and make all those tiles/cells where
-    // the nodes are rendered on -> an obstacle
+    //    -> if coordinates of a state is inside a specific cell then mark that cell as obstacle
 
     // Top-Left Corner (Given): (nodeX, nodeY)
-
-    // Top-Right Corner:
-    // Calculation: (nodeX + nodeWidth, nodeY)
-
-    // Bottom-Left Corner:
-    // Calculation: (nodeX, nodeY + nodeHeight)
-
-    // Bottom-Right Corner:
-    // Calculation: (nodeX + nodeWidth, nodeY + nodeHeight)
+    // Top-Right Corner: (nodeX + nodeWidth, nodeY)
+    // Bottom-Left Corner: (nodeX, nodeY + nodeHeight)
+    // Bottom-Right Corner: (nodeX + nodeWidth, nodeY + nodeHeight)
 
     // create the grid
     for (let x = 0; x < width; x++) {
@@ -42,30 +31,24 @@ export class Grid {
         let marked = "";
 
         nodeCoordinates.forEach((node) => {
-          const {
-            x: nodeX,
-            y: nodeY,
-            width: nodeWidth,
-            height: nodeHeight,
-          } = node;
+          const { x: nodeX, y: nodeY, width: nodeWidth, height: nodeHeight } = node;
 
           // get area of entire cell
           const cellRight = cellX + 10; // Assuming cellSize is 10
           const cellBottom = cellY + 10;
 
           // Check if any part of the node intersects with the cell's area
-          if (nodeX/10 < cellRight && (nodeX + nodeWidth)/10 > cellX 
-          && nodeY/10 < cellBottom && (nodeY + nodeHeight)/10 > cellY) {
-                console.log("object");
-              marked = "OBSTACLE";
+          if (nodeX / 10 < cellRight && (nodeX + nodeWidth) / 10 > cellX && nodeY / 10 < cellBottom && (nodeY + nodeHeight) / 10 > cellY) {
+            console.log("object");
+            marked = "OBSTACLE";
           }
-          
         });
 
         this.grid[x][y] = new Cell(context, x, y, 100, 100, marked);
       }
     }
   }
+  
 
   // Method to get a cell at a specific coordinate
   getCell(x, y) {
