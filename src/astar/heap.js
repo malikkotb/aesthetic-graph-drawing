2fde7da6a69ef80cb720fbaca8f74392aa49d151
Malik Kotb <malikkotb@icloud.com>
2024-03-07 12:26:53 +0100

    updateItem and containsItem

--- src/astar/heap.js
+++ src/astar/heap.js
@@ -4,19 +4,23 @@ class HeapItem {
     this.heapIndex = 0; // Initialized heap index
   }
 
-  
-    compareTo(otherItem) {
-        // this.value being the f_cost
-        // if this.value has higher priority than otherItem then it returns 1, if it has the same priority -> returns 0, if its got lower priority it returns -1
+  compareTo(otherItem) {
+    // this.value being the f_cost
+    // if this.value has higher priority than otherItem then it returns 1, if it has the same priority -> returns 0, if its got lower priority it returns -1
 
-        if (this.value < otherItem.value) {
-            return -1; // This instance is less than otherItem
-        } else if (this.value > otherItem.value) {
-            return 1; // This instance is greater than otherItem
-        } else {
-            return 0; // This instance is equal to otherItem
-        }
+    if (this.value < otherItem.value) {
+      return -1; // This instance is less than otherItem
+    } else if (this.value > otherItem.value) {
+      return 1; // This instance is greater than otherItem
+    } else {
+      return 0; // This instance is equal to otherItem
     }
+  }
+
+  equals(other) {
+    // Check if 'other' is an instance of HeapItem and has the same value
+    return other instanceof HeapItem && this.value === other.value && this.heapIndex === other.heapIndex;
+  }
 }
 
 export class Heap {
@@ -39,50 +43,69 @@ export class Heap {
     this.currentItemCount++; // increment currentItemCount
   }
 
+  updateItem(item) {
+    // in case I want to change the priority of an item
+    // i.e. in Pathfinding, where I might find a cell in the openSet which I want to update with a lower f_cost bc. I've found a new path to it
+    // -> need to be able to update its position in the heap
+    
+    // if the priority has been increased -> call sortUp()
+    sortUp(item);
+    // in pathfinding you'll only ever increase the priority and not decrease it -> no need to call sortDown()
+
+  }
+
+  count() {
+    return this.currentItemCount;
+  }
+
   removeFirstItem() {
     let firstItem = this.items[0];
     this.currentItemCount--;
 
-    // now I need to take the item at the end of the heap and put it into the first place 
-    this.items[0] = this.items[this.currentItemCount]
+    // now I need to take the item at the end of the heap and put it into the first place
+    this.items[0] = this.items[this.currentItemCount];
     this.items[0].heapIndex = 0;
-    this.sortDown(this.items[0])
+    this.sortDown(this.items[0]);
     return firstItem;
   }
 
-  sortDown(heapItem) {
-    while(true) {
-        let leftChildIndex = heapItem.heapIndex * 2 + 1;
-        let rightChildIndex = heapItem.heapIndex * 2 + 2;
-        let swapIndex = 0;
+  contains(item) {
+    // method to check if heap contains specific item
+    return items[item.heapIndex].equals(item);
+  }
 
-        // check if item has at least one child (the child on the left)
-        if (leftChildIndex < this.currentItemCount) {
-            swapIndex = leftChildIndex;
+  sortDown(item) {
+    while (true) {
+      let leftChildIndex = item.heapIndex * 2 + 1;
+      let rightChildIndex = item.heapIndex * 2 + 2;
+      let swapIndex = 0;
 
-            // then check if item also has right child 
-            if (rightChildIndex < this.currentItemCount) {
-                // then need to check which of the 2 children has got a higher priority and set the swapIndex to that child
-                if (this.items[leftChildIndex].compareTo(this.items[rightChildIndex]) < 0) {
-                    // if leftChildIndex has a lower priority than rightChildIndex 
-                    swapIndex = rightChildIndex;
-                    // now swapIndex is equal to the child with the highest priority 
-                }
-            }
+      // check if item has at least one child (the child on the left)
+      if (leftChildIndex < this.currentItemCount) {
+        swapIndex = leftChildIndex;
 
-            // check if parent has lower priority than its highest priority child in which case -> swap them 
-            if (heapItem.compareTo(items[swapIndex]) < 0) {
-                this.swapItems(heapItem, items[swapIndex]);
-            } else {
-                // if parent has higher priority than both of its children, then its in its correct position
-                return;
-            }
-        } else {
-            // parent doesnt have any children, then its in its correct position
-            return; 
+        // then check if item also has right child
+        if (rightChildIndex < this.currentItemCount) {
+          // then need to check which of the 2 children has got a higher priority and set the swapIndex to that child
+          if (this.items[leftChildIndex].compareTo(this.items[rightChildIndex]) < 0) {
+            // if leftChildIndex has a lower priority than rightChildIndex
+            swapIndex = rightChildIndex;
+            // now swapIndex is equal to the child with the highest priority
+          }
         }
+
+        // check if parent has lower priority than its highest priority child in which case -> swap them
+        if (item.compareTo(items[swapIndex]) < 0) {
+          this.swapItems(item, items[swapIndex]);
+        } else {
+          // if parent has higher priority than both of its children, then its in its correct position
+          return;
+        }
+      } else {
+        // parent doesnt have any children, then its in its correct position
+        return;
+      }
     }
-    
   }
 
   // method to maintain heap property after adding an item
@@ -95,7 +118,7 @@ export class Heap {
         // if heapItem has higher priority than parentItem (which in this case means it has a lower f_cost) -> then swap these items
         this.swapItems(heapItem, parentItem);
       } else {
-        break; // as soon as the item is no longer of a higher priority than its parentItem -> break out of loop   
+        break; // as soon as the item is no longer of a higher priority than its parentItem -> break out of loop
       }
       parentIndex = Math.floor((heapItem.heapIndex - 1) / 2);
     }
